ae1018785359a768427abcf7b7ac5177ca007af8
Md. Muied Hossain <md.muiedhossain@gmail.com>
2023-12-12 20:11:33 +0600

    favourite list done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -55,6 +55,7 @@ class _HomePageState extends State<HomePage> {
             setState(() {
               selectedIndex = index;
             });
+            _fetchData();
           },
           currentIndex: selectedIndex,
           backgroundColor: Colors.white60,
@@ -155,5 +156,15 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ));
+  }
+  void _fetchData() {
+    switch(selectedIndex) {
+      case 0:
+        Provider.of<ContactProvider>(context, listen: false).getAllContacts();
+        break;
+      case 1:
+        Provider.of<ContactProvider>(context, listen: false).getAllFavoriteContacts();
+        break;
+    }
   }
 }
