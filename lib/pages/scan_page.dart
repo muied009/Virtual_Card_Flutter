16c3bf5887a31baeb3f7b1c3e8b81fa44f3cb3db
Md. Muied Hossain <md.muiedhossain@gmail.com>
2023-12-12 18:16:44 +0600

    drag and drop done

--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:visiting_card/utils/constants.dart';
 
 class ScanPage extends StatefulWidget {
   static const String routeName = "/scan";
@@ -33,6 +34,23 @@ class _ScanPageState extends State<ScanPage> {
               TextButton.icon(onPressed: (){getImage(ImageSource.camera);}, icon: const Icon(Icons.camera_alt), label: const Text("Capture")),
               TextButton.icon(onPressed: (){getImage(ImageSource.gallery);}, icon: const Icon(Icons.photo), label: const Text("Gallery"))
             ],
+          ),
+         if(isScanOver)  Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  DropTargetItem(property: ContactProperties.name, onDrop: _getPropertyValue),
+                  DropTargetItem(property: ContactProperties.designation, onDrop: _getPropertyValue),
+                  DropTargetItem(property: ContactProperties.companyName, onDrop: _getPropertyValue),
+                  DropTargetItem(property: ContactProperties.address, onDrop: _getPropertyValue),
+                  DropTargetItem(property: ContactProperties.email, onDrop: _getPropertyValue),
+                  DropTargetItem(property: ContactProperties.mobile, onDrop: _getPropertyValue),
+                  DropTargetItem(property: ContactProperties.website, onDrop: _getPropertyValue),
+                ],
+              ),
+            ),
           ),
           Wrap(
             direction: Axis.horizontal,
@@ -68,7 +86,66 @@ class _ScanPageState extends State<ScanPage> {
       print("jkjk$lines");
     }
   }
+
+  _getPropertyValue(String property, String value) {
+  }
 }
+
+class DropTargetItem extends StatefulWidget {
+  final String property;
+  final Function onDrop;
+  const DropTargetItem({super.key, required this.property, required this.onDrop});
+
+  @override
+  State<DropTargetItem> createState() => _DropTargetItemState();
+}
+
+class _DropTargetItemState extends State<DropTargetItem> {
+  String draggedItem = "";
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(flex: 1, child: Text(widget.property)),
+        Expanded(
+          flex: 2,
+          child: DragTarget<String>(
+              builder: (context,candidateData, rejectedData) => Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  border: candidateData.isNotEmpty ? Border.all(color: Colors.red,width: 2) : null,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(child: Text(draggedItem.isEmpty ? "Drop Here" : draggedItem)),
+                    if (draggedItem.isNotEmpty)
+                      InkWell(
+                        onTap: (){
+                          setState(() {
+                            draggedItem = "";
+                          });
+                        },
+                          child: const Icon(Icons.clear, size: 15))
+                  ],
+                ),
+              ),
+            onAccept: (value){
+                setState(() {
+                  if(draggedItem.isEmpty){
+                    draggedItem = value;
+                  }else{
+                    draggedItem += " $value";
+                  }
+                });
+                widget.onDrop(widget.property,draggedItem);
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
 
 class LineItem extends StatelessWidget {
   final String line;
---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+abstract final class ContactProperties {
+  static const String name = "Name";
+  static const String mobile = "Mobile";
+  static const String email = "Email";
+  static const String address = "Address";
+  static const String companyName = "Company";
+  static const String designation = "Designation";
+  static const String website = "Website";
+}
+
+const String dragInstruction = 'Long press and Drag each item from the below list and drop above in the appropriate box. You can drop multiple items over a single box.';
