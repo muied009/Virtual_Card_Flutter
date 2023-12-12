0b3a8493e3db0d5907fb31ee933a89db21664ef3
Md. Muied Hossain <md.muiedhossain@gmail.com>
2023-12-12 19:42:55 +0600

    a

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,12 +23,13 @@ class _HomePageState extends State<HomePage> {
   bool isFirst = true;
   @override
   void didChangeDependencies() {
-    if(isFirst) {
-      Provider.of<ContactProvider>(context,listen: false).getAllContacts();
+    if (isFirst) {
+      Provider.of<ContactProvider>(context, listen: false).getAllContacts();
     }
     isFirst = false;
     super.didChangeDependencies();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,91 +37,97 @@ class _HomePageState extends State<HomePage> {
         title: const Text("My Contacts"),
         elevation: 1,
       ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          shape: const CircleBorder(),
-          onPressed: () {
-            Navigator.pushNamed(context, ScanPage.routeName);
-          },
-          child: const Icon(Icons.add),
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        onPressed: () {
+          Navigator.pushNamed(context, ScanPage.routeName);
+        },
+        child: const Icon(Icons.add),
+      ),
       bottomNavigationBar: BottomAppBar(
         padding: EdgeInsets.zero,
         shape: const CircularNotchedRectangle(),
         notchMargin: 10,
         clipBehavior: Clip.antiAlias,
         child: BottomNavigationBar(
-          onTap: (index){
+          onTap: (index) {
             setState(() {
               selectedIndex = index;
             });
-
           },
           currentIndex: selectedIndex,
           backgroundColor: Colors.white60,
           selectedItemColor: Colors.green,
           unselectedItemColor: Colors.grey,
           items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "All"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.person), label: "All"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.favorite), label: "Favourite"
-            ),
+                icon: Icon(Icons.favorite), label: "Favourite"),
           ],
         ),
       ),
       body: Consumer<ContactProvider>(
-        builder: (context,provider,_){
-          if (provider.contactList.isEmpty){
+        builder: (context, provider, _) {
+          if (provider.contactList.isEmpty) {
             return const Center(
               child: Text("Nothing to show"),
             );
-          }else{
+          } else {
             return ListView.builder(
-            itemCount: provider.contactList.length,
-                itemBuilder: (context,index){
-              final contact = provider.contactList[index];
-              return Dismissible(
-                ///for swipable delete
-                key: ValueKey(contact.id),
-                  background: Container(
-                    padding: const EdgeInsets.only(right: 20),
+                itemCount: provider.contactList.length,
+                itemBuilder: (context, index) {
+                  final contact = provider.contactList[index];
+                  return Dismissible(
+                    ///for swipable delete
+                    key: ValueKey(contact.id),
+                    background: Container(
+                      padding: const EdgeInsets.only(right: 20),
                       alignment: Alignment.centerRight,
                       color: Colors.red,
-                      child: const Icon(Icons.delete,color: Colors.white,),
-                  ),
-                  confirmDismiss: _showConfirmationDialog,
-                  onDismissed: (direction) async {
-
-                  ///onDismissed tokhoni call hobe jokhon "_showConfirmationDialog" eta true return kobe
-                  await provider.deleteContact(contact.id);
-                  showMsg(context, 'Deleted');
-
-                  },
-                  direction: DismissDirection.endToStart,
-                child: ListTile(
-                  ///navigator = (context, kun page e jabo, ki niye jabo parameter hisebe)
-                onTap: () => Navigator.pushNamed(context, ContactDetailsPage.routeName,arguments: contact.id),
-                  title: Text(contact.name,style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold
-                  ),),
-                    subtitle: Text(contact.mobile,style: const TextStyle(
-                        fontSize: 11,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold
-                    ),),
-                  trailing: IconButton(
-                    onPressed: (){
-                      provider.updateContactField(contact, tblContactColFavorite);
+                      child: const Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                    confirmDismiss: _showConfirmationDialog,
+                    onDismissed: (direction) async {
+                      ///onDismissed tokhoni call hobe jokhon "_showConfirmationDialog" eta true return kobe
+                      await provider.deleteContact(contact.id);
+                      showMsg(context, 'Deleted');
                     },
-                    icon: Icon(contact.favorite ? Icons.favorite : Icons.favorite_border),
-                  ),
-                ),
-              );
-                }
-            );
+                    direction: DismissDirection.endToStart,
+                    child: ListTile(
+                      ///navigator = (context, kun page e jabo, ki niye jabo parameter hisebe)
+                      onTap: () => Navigator.pushNamed(
+                          context, ContactDetailsPage.routeName,
+                          arguments: contact.id),
+                      title: Text(
+                        contact.name,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        contact.mobile,
+                        style: const TextStyle(
+                            fontSize: 11,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          provider.updateContactField(
+                              contact, tblContactColFavorite);
+                        },
+                        icon: Icon(contact.favorite
+                            ? Icons.favorite
+                            : Icons.favorite_border),
+                      ),
+                    ),
+                  );
+                });
           }
         },
       ),
@@ -128,23 +135,25 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<bool?> _showConfirmationDialog(DismissDirection direction) {
-    return showDialog(context: context, builder: (context) => AlertDialog(
-      title: const Text('Delete Contact'),
-      content: const Text('Are you sure to delete this contact?'),
-      actions: [
-        OutlinedButton(
-          onPressed: () {
-            Navigator.pop(context, false);
-          },
-          child: const Text('NO'),
-        ),
-        OutlinedButton(
-          onPressed: () {
-            Navigator.pop(context, true);
-          },
-          child: const Text('YES'),
-        ),
-      ],
-    ));
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Delete Contact'),
+              content: const Text('Are you sure to delete this contact?'),
+              actions: [
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  child: const Text('NO'),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  child: const Text('YES'),
+                ),
+              ],
+            ));
   }
 }
